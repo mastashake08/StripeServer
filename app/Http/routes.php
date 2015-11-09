dd49f96f00b5998e7acfdc0fcfc804cceca2d233
Jyrone Parker <jyrone.parker@gmail.com>
2015-11-09 14:17:35 -0800

    Switch statement to handle webhooks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,13 +17,25 @@ $app->get('/', function () use ($app) {
     return view('index');
 });
 
-$app->post('success', function (Request $request) {
+$app->post('stripe', function (Request $request) {
+  switch($request->type){
+    case 'charge.succeeded':
      $number = $request->data['object']['amount'] / 100;
     Mail::raw('You got paid $'. money_format('%i', $number) . "\n",
     function($msg) {
       $msg->to(['8594024863@messaging.sprintpcs.com']);
        $msg->from(['payment@jyroneparker.com']);
      });
+     break;
+     case 'charge.failed':
+     $number = $request->data['object']['amount'] / 100;
+    Mail::raw('Charge failed in amount of $'. money_format('%i', $number) . "\n",
+    function($msg) {
+      $msg->to(['8594024863@messaging.sprintpcs.com']);
+       $msg->from(['payment@jyroneparker.com']);
+     });
+     break;
+   }
 });
 
 $app-> post('charge',['middleware' => 'cors', function(Request $request){

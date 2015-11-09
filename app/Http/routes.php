40a75d863dca9be767d5e76cd031372950bc70e4
Jyrone Parker <jyrone.parker@gmail.com>
2015-11-09 13:53:37 -0800

    Show request data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ $app->get('/', function () use ($app) {
 });
 
 $app->post('success', function (Request $request) {
-    return $request->data[0]->amount;
+     dd($request->all());
     /*Mail::raw('You got paid',
     function($msg) {
       $msg->to(['8594024863@messaging.sprintpcs.com']);

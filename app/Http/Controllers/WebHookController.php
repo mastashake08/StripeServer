2bb92e266c015366939f01698a8a047b0117b5e2
Jyrone Parker <jyrone.parker@gmail.com>
2016-03-23 03:30:53 +0000

    Add github handle

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -31,7 +31,7 @@ public function handleStripe(Request $request){
 
 public function handleGithub(Request $request){
 
-    Mail::raw('There is a new push to the project: '. $request->all(),
+    Mail::raw('There is a new push to the project: '. serialize($request->all()),
     function($msg) {
       $msg->to('8594024863@messaging.sprintpcs.com');
       $msg->cc('5026441212@tmomail.net');
